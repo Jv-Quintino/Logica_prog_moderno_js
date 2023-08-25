bbbb71f30741a0c78b7d69cda7dd8c413ef0a540
João Vitor Q. Santos <139080343+Jv-Quintino@users.noreply.github.com>
2023-08-25 13:11:18 -0300

    Curso de Lógica de Prog. em JavaScript + NodeJs

---
+++ aula2.js
@@ -0,0 +1,20 @@
+let x = 5
+let y = 7
+
+//OPERADORES ARITMÉTRICOS
+let resultado = x + y 
+//ou let resultado = x - y SUBTRAÇÃO
+//ou let resultado = x * y MULTIPLICAÇÃO
+//ou let resultado = x / y DIVISÃO
+//ou let resultado = x % y MÓDULO
+
+//OPERADORES LÓGICOS 
+//let resultado = x == y COMPARANDO PARA VER SE SÃO IGUAIS
+//ou let resultado = x != y COMPARANDO PARA VER SE SÃO DIFERENTES
+//ou let resultado = x > y COMPARANDO PARA VER SE É MAIOR  
+//ou let resultado = x >= y COMPARANDO PARA VER SE É MAIOR OU IGUAL
+//ou let resultado = x < y COMPARANDO PARA VER SE É MENOR
+//ou let resultado = x <= y COMPARANDO PARA VER SE É MENOR OU IGUAL
+   
+
+console.log("Resposta: " + resultado)
---
+++ condicionais.js
@@ -0,0 +1,14 @@
+let nota = 80
+let frequencia = 30
+
+if(nota >= 6 && frequencia >= 75 ) {
+  console.log("Aprovado!! Nota: " + nota)
+  console.log("Aprovado!! Frequência de aulas: " + frequencia)
+} else {
+  console.log("REPROVADO!! Sua nota ou sua frequência não alcançou a média para aprovação!!")
+  console.log("Nota: " + nota)
+  console.log("Frequência: " + frequencia)
+}
+//CONDICIONAIS: 
+// && = e 
+// || = ou
