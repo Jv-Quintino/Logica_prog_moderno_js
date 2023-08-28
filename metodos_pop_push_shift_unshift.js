6fbf758462ad93afcdf2aeedcc653de3be0acb0e
João Vitor Q. Santos <139080343+Jv-Quintino@users.noreply.github.com>
2023-08-28 12:05:30 -0300

    Mais atualizações e exercícios.

---
+++ metodos_pop_push_shift_unshift.js
@@ -0,0 +1,18 @@
+/*let v = ['Cadeira', 'Tv', 'Sofá']
+console.log(v)
+    //v.pop()           //Exclui o ultimo registro do vetor 
+    //v.push('Mesa')    //Adiciona registro na ultima casa do vetor
+    //v.shift()         //Apaga o primeiro registro de um vetor 
+    //v.unshift('Mesa') //Adiciona o registro na primeira casa do vetor 
+console.log(v)*/
+
+//Podemos ultilizar os metodos em funções:
+
+function add(x){
+    v.push(x)
+    
+}
+let v = ['Real Madrid', 'Barcelona']
+
+add('Milan')
+console.log(v)
---
+++ metodos_splice.js
@@ -0,0 +1,5 @@
+let v = ['Ceará', 'João Pessoa', 'Alagoas']
+console.log(v)
+v.splice(1,1)//primeiro informa a casa do vetor, segundo o numero de casas que serão apagadas.
+//para adicionar v.splice(0,0, 'São Paulo')
+console.log(v)
